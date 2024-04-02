ceaace8ea5a549d63d640436f20ca4156ef10adc
Elisandro Franco <100567537+ElisandroF07@users.noreply.github.com>
2024-04-02 08:53:43 +0100

    Update page.tsx

--- src/pages/forgot-credentials/page.tsx
+++ src/pages/forgot-credentials/page.tsx
@@ -1,72 +1,84 @@
 "use client";
 
+import axios from "axios";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
+import { log } from "console";
 import InfoError from "@/components/others/infoError";
 import useUserStore from "@/contexts/stores/userStore";
-import business from "@/assets/images/Forgot password-pana.svg";
 import "@/styles/globals.css";
 import "@/styles/phone.css";
-import api from "@/services/api";
-import { TailSpin } from 'react-loader-spinner'
+import business from "@/assets/images/Upvote-pana.svg";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { TailSpin } from "react-loader-spinner";
 
 const FormSchema = z.object({
-	email: z
-		.string()
-		.min(1, "O email é obrigatório!")
-		.email("Email inválido! Corrija o email")
-		.transform((email) => {
-			return email.trim().toLowerCase();
-		}),
-});
+	accessCode: z.string()
+		.min(1, "O campo não pode estar vazio!")
+		.regex(/^[0-9]{6}$/, "O código de acesso deve ser composto por 6 digitos!"),
+	confirmAccessCode: z.string()
+		.min(1, "O campo não pode estar vazio!")
+		.regex(/^[0-9]{6}$/, "O código de acesso deve ser composto por 6 digitos!"),
+	})
+	.refine((data) => data.accessCode === data.confirmAccessCode, {
+		message: "Os campos deves ser iguais",
+		path: ["confirmAccessCode"],
+	});
 
 type FormType = z.infer<typeof FormSchema>;
 
-export default function ForgotPassword() {
-	const router = useRouter();
+export default function SetCredentials() {
+
 	const useStore = useUserStore();
-	const [loading, setLoading] = useState(false);
+	const router = useRouter()
+	const [loading, setLoading] = useState(false)
+
+	let email_local = "";
+	if (typeof window !== "undefined") {
+		email_local = localStorage.getItem("email") ?? useStore.email
+	}
 
 	const {register, formState: { errors }, handleSubmit} = useForm<FormType>({
 		resolver: zodResolver(FormSchema),
 	});
 
-	function APICall(data: FormType): Promise<string> {
-		setLoading(true);
-		const { email } = data;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	function APICall(data: any): Promise<string> {
+		setLoading(true)
 		// biome-ignore lint/suspicious/noAsyncPromiseExecutor: <explanation>
 		return new Promise(async (resolve, reject) => {
 			try {
-				const response = await api.get(`/resetPassword/${email}`);
+				const response = await axios.post("http://localhost:5000/setAccessCode", data, { headers: { "Content-Type": "application/json" } });
 				if (response.status === 201) {
-					useStore.updateEmail(email);
-					if (typeof window !== "undefined") {
-						localStorage.setItem("email", email);
-					}
+					router.push("/login")
 					resolve(response.data.message);
-				} else {
-					reject(response.data.message);
-					setLoading(false);
 				}
+				reject(response.data.message);
+				setLoading(false)
 			} 
 			catch {
-				reject("Não foi possivel processar a sua solicitação! Verifique a sua conexão com a interent.");
-				setLoading(false);
-			} 
+				reject("Não foi possivel processar a sua solicitação! Verifique a sua conexão com a internet.");
+				setLoading(false)
+			}
 		});
 	}
 
 	async function submitForm(data: FormType) {
-		toast.promise(APICall(data), {
+		const { accessCode } = data;
+		const email = email_local || useStore.email;
+		const formatedData = JSON.stringify({
+			accessCode,
+			email
+		});
+
+		toast.promise(APICall(formatedData), {
 			loading: "Enviando...",
 			success: (data) => {
-				router.push("/email-verification");
 				return data;
 			},
 			error: (data) => {
@@ -80,29 +92,44 @@ export default function ForgotPassword() {
 			<div className="home_body">
 				<div className="left">
 					<Image src={business} alt="business" />
-					<h1>Esqueceu o seu código de acesso?</h1>
-					<p>Não faça pranto! Recupere em simples passos.</p>
+					<h1>Defina um novo código de acesso</h1>
+					<p>
+						O código de acesso de ve ser constituido por uma combinação de 6
+						digitos.
+					</p>
 				</div>
 				<div className="right">
 					<form onSubmit={handleSubmit(submitForm)} className="login_form">
 						<div className="header_form">
 							<h1>Recuperação</h1>
-							<p>
-								Já lembrou o seu código de acesso?{" "}
-								<Link href={"/login"}>Fazer login</Link>
-							</p>
+							<p>Crie e confirme o seu novo código de acesso</p>
 						</div>
 						<div className="body_form">
 							<div className="input_field">
-								<label htmlFor="email">Endereço de email</label>
+								<label htmlFor="membershipNumber">Novo código de acesso</label>
 								<input
-									type="text"
-									placeholder="Insira o seu endereço de email "
-									{...register("email")}
+									type="password"
+									placeholder="Insira o seu novo código de acesso "
+									{...register("accessCode")}
 								/>
-								{errors.email && <InfoError message={errors.email.message} />}
+								{errors.accessCode && (
+									<InfoError message={errors.accessCode.message} />
+								)}
 							</div>
-							<button type="submit" disabled={loading} className="button_auth">
+							<div className="input_field">
+								<label htmlFor="membershipNumber">
+									Confirme o novo código de acesso
+								</label>
+								<input
+									type="password"
+									placeholder="Insira novamente o seu novo código de acesso "
+									{...register("confirmAccessCode")}
+								/>
+								{errors.confirmAccessCode && (
+									<InfoError message={errors.confirmAccessCode.message} />
+								)}
+							</div>
+							<button type="submit" className="button_auth" disabled={loading}>
 							{loading ? (
 								<TailSpin
 									height="25"
@@ -113,7 +140,7 @@ export default function ForgotPassword() {
 									visible={true}
 								/>
 							) : (
-								'Verificar email'
+								'Salvar'
 							)}
 							</button>
 							<div className="terms">
