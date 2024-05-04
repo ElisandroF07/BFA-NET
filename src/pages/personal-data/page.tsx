db479dc1f607e5404febad3951f61bac7d1f924a
Elisandro Franco <100567537+ElisandroF07@users.noreply.github.com>
2024-05-04 09:38:11 +0100

    Update page.tsx

--- src/pages/personal-data/page.tsx
+++ src/pages/personal-data/page.tsx
@@ -10,6 +10,8 @@ import { useForm } from "react-hook-form";
 import InfoError from "@/components/others/infoError";
 import useUserStore from "@/contexts/stores/userStore";
 import { TailSpin } from 'react-loader-spinner'
+import useStepsStore from "@/contexts/stores/stepsStore";
+import api from "@/services/api";
 
 const FormSchema = z.object({
 	name: z.string({required_error: "O campo não pode estar vazio!"})
@@ -48,67 +50,125 @@ const FormSchema = z.object({
 		}),
 });
 
+const FormSchema2 = z.object({
+	name: z.string({required_error: "O campo não pode estar vazio!"})
+		.min(1, "O campo não pode estar vazio!")
+		.transform((name) => {
+			return name
+				.trim()
+				.split(" ")
+				.map((word) => {return word[0].toLocaleUpperCase().concat(word.substring(1));});
+		}),
+	birthDate: z
+		.string()
+		.refine(
+			(value) => {
+				const date = new Date(value);
+				return !Number.isNaN(date.getTime());
+			},
+			{
+				message:
+					"A data de nascimento deve estar no formato válido (DD/MM/AAAA)",
+				path: ["birthDate"],
+			},
+		)
+		.transform((value) => {
+			return new Date(value);
+		}),
+
+	biNumber: z
+		.string({
+			required_error: "O número do BI é obrigatório!",
+		})
+		.min(1, "O número do BI é obrigatório!")
+		.regex(/^[0-9]{9}[A-Z|a-z]{2}[0-9]{3}$/, "Número do BI inválido!")
+		.transform((phone) => {
+			return phone.trim().toUpperCase();
+		}),
+	local: z.string({required_error: "O campo é obrigatório!"})
+		.min(1, "O campo é obrigatório!"),
+	area: z.string({required_error: "O campo é obrigatório!"})
+		.min(1, "O campo é obrigatório!")
+});
+
 type FormType = z.infer<typeof FormSchema>;
+type FormType2 = z.infer<typeof FormSchema2>;
 
 export default function PersonalData() {
 
 	const router = useRouter();
 	const [loading, setLoading] = useState(false);
 	const useStore = useUserStore();
+	const useSteps = useStepsStore()
 	let email_address = "";
+	let account_type = "";
 	if (typeof window !== "undefined") {
 		email_address = localStorage.getItem("email") ?? useStore.email
+		account_type = localStorage.getItem("accountType") ?? ""
 	}
+
+	useEffect(()=>{
+		useSteps.setCurrent(1)
+		useSteps.setStep1(true)
+	}, [])
 
 	const {register, handleSubmit, formState: { errors }} = useForm<FormType>({
 		resolver: zodResolver(FormSchema),
 	});
 
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	async  function APICall(data: any): Promise<string> {
+	const {register: register2, handleSubmit: handleSubmit2, formState: { errors: errors2 }} = useForm<FormType2>({
+		resolver: zodResolver(FormSchema2),
+	});
+
+	async function APICall(data: any) {
 		setLoading(true);
-		// biome-ignore lint/suspicious/noAsyncPromiseExecutor: <explanation>
-		return new Promise(async (resolve, reject) => {
 			try {
-				const response = await axios.post("https://bfa-nodejs-api.onrender.com/personal-data",data,{ headers: { "Content-Type": "application/json" } });
+				const response = await api.post("/personal-data", data);
 				if (response.status === 201) {
 					router.push("/register/identity-validation");
-					resolve(response.data.message);
-				} else {
-					reject(response.data.message);
+					toast.success(response.data.message);
+				} 
+				else {
+					toast.error(response.data.message);
 					setLoading(false);
 				}
 			} catch {
-				reject("Não foi possivel processar a sua solicitação! Verifique a sua conexão com a internet.");
+				toast.error("Sem conexão com o servidor!");
 				setLoading(false);
 			} 
-		});
 	}
 
 	async function submitForm(data: FormType) {
-		const { name, birthDate, biNumber } = data;
-		const parsedBirthDate = new Date(birthDate);
+		const parsedBirthDate = new Date(data.birthDate);
 		const email = email_address ?? useStore.email;
 		const formatedData = JSON.stringify({
-			name,
-			biNumber,
+			name: data.name,
+			biNumber: data.biNumber,
 			email,
 			birthDate: parsedBirthDate,
 		});
 
-		toast.promise(APICall(formatedData), {
-			loading: "Enviando...",
-			success: (data) => {
-				return data;
-			},
-			error: (data) => {
-				return data;
-			},
-		});
+		APICall(formatedData)
 	}
 
-	return (
-		<form onSubmit={handleSubmit(submitForm)} className="login_form">
+	async function submitForm2(data: FormType2) {
+		const parsedBirthDate = new Date(data.birthDate);
+		const email = email_address ?? useStore.email;
+		const formatedData = JSON.stringify({
+			name: data.name,
+			biNumber: data.biNumber,
+			email,
+			birthDate: parsedBirthDate,
+		});
+		if (typeof window !== "undefined") {
+			localStorage.setItem("local", data.local)
+			localStorage.setItem("area", data.area)
+		}
+		APICall(formatedData)
+	}
+
+	return account_type === "c1" ? 
+		(<form onSubmit={handleSubmit(submitForm)} className="login_form">
 			<div className="header_form">
 				<h1>Dados pessoais</h1>
 				<p>
@@ -159,6 +219,77 @@ export default function PersonalData() {
 					)}
 				</button>
 			</div>
-		</form>
-	);
+		</form> )
+		: 
+		(<form onSubmit={handleSubmit2(submitForm2)} className="login_form">
+			<div className="header_form">
+				<h1>Dados pessoais</h1>
+				<p>
+					Introduza os seus dados pessoais abaixo. Certifique-se de digitar as
+					informações corretamente.
+				</p>
+			</div>
+			<div className="body_form">
+				<div className="input_field">
+					<label htmlFor="name">Nome completo</label>
+					<input
+						type="text"
+						placeholder="Insira o seu nome completo"
+						{...register2("name")}
+					/>
+					{errors2.name && <InfoError message={errors2.name.message} />}
+				</div>
+				<div className="input_field">
+					<label htmlFor="email">Data de nascimento</label>
+					<input
+						type="date"
+						placeholder="Insira a sua data de nascimento "
+						{...register2("birthDate")}
+					/>
+					{errors2.birthDate && <InfoError message={errors2.birthDate.message} />}
+				</div>
+				<div className="input_field">
+					<label htmlFor="bi_number">Número do BI</label>
+					<input
+						type="text"
+						placeholder="Insira o número do BI"
+						{...register2("biNumber")}
+					/>
+					{errors2.biNumber && <InfoError message={errors2.biNumber.message} />}
+				</div>
+				<div className="input_field">
+					<label htmlFor="bi_number">Área de atividade</label>
+					<input
+						type="text"
+						placeholder="Insira a área de atividade do seu negócio"
+						{...register2("area")}
+					/>
+					{errors2.area && <InfoError message={errors2.area.message} />}
+				</div>
+				<div className="input_field">
+					<label htmlFor="bi_number">Local do estabelecimento</label>
+					<input
+						type="text"
+						placeholder="Insira o local do seu estabelecimento"
+						{...register2("local")}
+					/>
+					{errors2.local && <InfoError message={errors2.local.message} />}
+				</div>
+				<button type="submit" disabled={loading} className="button_auth">
+					{loading ? (
+						<TailSpin
+							height="25"
+							width="25"
+							color="#fff"
+							ariaLabel="tail-spin-loading"
+							radius="1"
+							visible={true}
+						/>
+					) : (
+						'Enviar dados'
+					)}
+				</button>
+			</div>
+		</form>)
+
 }
